1ad1353bf13e6d67ef4ce07bcd78630ef9bacf6a
Souvik Biswas <sbis1999@gmail.com>
2021-03-19 18:40:59 +0530

    Add some files

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text('helloo1'),
+          Text('helloo2'),
+        ],
+      ),
+    );
+  }
+}
