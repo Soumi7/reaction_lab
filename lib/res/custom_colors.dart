2c6d4ab1f91bf0e014d11ee1ef45da561a278c94
Souvik Biswas <sbis1999@gmail.com>
2021-03-20 11:20:27 +0530

    Made custom colors static

--- lib/res/custom_colors.dart
+++ lib/res/custom_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  final Color primaryAccent = Color(0xFF6AE2CD);
-  final Color primaryDark = Color(0xFF181A4B);
-  final Color orangeDark = Color(0xFFFF5A49);
-  final Color orangeLight = Color(0xFFFF855F);
-  final Color yellow = Color(0xFFFFDB8F);
+  static final Color primaryAccent = Color(0xFF6AE2CD);
+  static final Color primaryDark = Color(0xFF181A4B);
+  static final Color orangeDark = Color(0xFFFF5A49);
+  static final Color orangeLight = Color(0xFFFF855F);
+  static final Color yellow = Color(0xFFFFDB8F);
 }
