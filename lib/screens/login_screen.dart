eeaed61892b5a3464ab6066407444429aaa4c40a
Souvik Biswas <sbis1999@gmail.com>
2021-03-20 11:21:03 +0530

    Update login screen :rocket:

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,43 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:reaction_lab/res/custom_colors.dart';
+import 'package:reaction_lab/widgets/login_screen/google_sign_in_button.dart';
 
-import 'dashboard_screen.dart';
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
 
-class LoginScreen extends StatelessWidget {
+class _LoginScreenState extends State<LoginScreen> {
+  // final FocusNode _emailFocusNode = FocusNode();
+  // final FocusNode _passwordFocusNode = FocusNode();
+
+  // Future<FirebaseApp> _initializeFirebase() async {
+  //   FirebaseApp firebaseApp = await Firebase.initializeApp();
+
+  //   User? user = FirebaseAuth.instance.currentUser;
+
+  //   if (user != null) {
+  //     Navigator.of(context).pushReplacement(
+  //       MaterialPageRoute(
+  //         builder: (context) => UserInfoScreen(
+  //           user: user,
+  //         ),
+  //       ),
+  //     );
+  //   }
+
+  //   return firebaseApp;
+  // }
   @override
   Widget build(BuildContext context) {
-    TextEditingController _emailController = TextEditingController();
-
     return Scaffold(
-      backgroundColor: Colors.pinkAccent,
-      appBar: AppBar(
-        title: Text('Flutter hello'),
-      ),
-      body: Padding(  
-            padding: EdgeInsets.all(40),  
-            child : Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _emailController,
+      backgroundColor: CustomColors.primaryAccent,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
           ),
-          SizedBox(
-            width: 100.0,
-            height: 30.0,
-            ),
-          ElevatedButton(
-            onPressed: () {
-              print("");
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DashboardScreen(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Image.asset(
+                        'assets/reaca_animated.gif',
+                        height: 300,
+                      ),
+                    ),
+                    // SizedBox(height: 20),
+                    Text(
+                      'Reaca',
+                      style: TextStyle(
+                        color: CustomColors.primaryDark,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
-            child: Text('Login'),
+              ),
+              GoogleSignInButton(),
+              // FutureBuilder(
+              //   future: Authentication.initializeFirebase(context: context),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.hasError) {
+              //       return Text('Error initializing Firebase');
+              //     } else if (snapshot.connectionState == ConnectionState.done) {
+              //       return GoogleSignInButton();
+              //     }
+              //     return CircularProgressIndicator(
+              //       valueColor: AlwaysStoppedAnimation<Color>(
+              //         CustomColors.firebaseOrange,
+              //       ),
+              //     );
+              //   },
+              // ),
+            ],
           ),
-        ],
+        ),
       ),
-      )
     );
   }
 }
