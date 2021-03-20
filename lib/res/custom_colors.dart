4fd7838dd9d545d401a90b13e10267a75f643bda
Souvik Biswas <sbis1999@gmail.com>
2021-03-20 10:35:55 +0530

    Add color palette

---
+++ lib/res/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  final Color primaryAccent = Color(0xFF6AE2CD);
+  final Color primaryDark = Color(0xFF181A4B);
+  final Color orangeDark = Color(0xFFFF5A49);
+  final Color orangeLight = Color(0xFFFF855F);
+  final Color yellow = Color(0xFFFFDB8F);
+}
