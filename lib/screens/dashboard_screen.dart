b410b91e777b80369f2f0d4b33eb8e25a42116f1
Souvik Biswas <sbis1999@gmail.com>
2021-03-20 16:53:10 +0530

    Add some TODO :sunglasses:

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,6 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
+  final User user;
+
+  // named constructors are defined inside { }, otherwise
+  // can also be defined without them (But named easy to use).
+  const DashboardScreen({required this.user});
+
+  /// TODO: Define the following inside a column:
+  /// * an Info area showing player stats
+  /// * then two buttons leading to:
+  ///   * PraticeGameScreen and,
+  ///   * MultiplayerScreen
+  ///
+  /// TIPS: you can define widgets as spearate files to keep
+  /// the code clean, like I have done inside `widgets --> login_screeen` folder
+  /// defined a file called `google_sign_in_button.dart`.
+  ///
+  /// `widgets` then the folder inside it is the `name of the screen` file.
+  ///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,21 +33,23 @@ class DashboardScreen extends StatelessWidget {
             "Click to enter Reaction Lab",
           ),
           ElevatedButton(
-              onPressed: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(builder : (context) => PraticeGameScreen()
-                //   ),
-                // );
-              },
-              child: Text('Practise Lab')),
+            onPressed: () {
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(builder : (context) => PraticeGameScreen()
+              //   ),
+              // );
+            },
+            child: Text('Practise Lab'),
+          ),
           ElevatedButton(
-              onPressed: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(builder : (context) => MultiplayerScreen()
-                //   ),
-                // );
-              },
-              child: Text('Enter Reaction Lab')),
+            onPressed: () {
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(builder : (context) => MultiplayerScreen()
+              //   ),
+              // );
+            },
+            child: Text('Enter Reaction Lab'),
+          ),
         ],
       ),
     );
