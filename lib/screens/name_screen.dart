390af42a12a93a9bba80812da9d46ea608b9f9c2
soumi7 <soumibardhan10@gmail.com>
2021-03-22 10:58:30 +0530

    experimenting :) !!

--- lib/screens/name_screen.dart
+++ lib/screens/name_screen.dart
@@ -10,21 +10,34 @@ class NameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: CustomColors.orangeLight,
+      backgroundColor: CustomColors.primaryAccent,
       appBar: AppBar(
         title: Text("Reaca"),
         centerTitle: true,
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 15.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(padding: const EdgeInsets.all(10.0),
               child: TextField(
+                decoration: InputDecoration(
+                hintText: 'Username',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12.0))
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+                ),
               )
+              ),
+              SizedBox(
+                height: 20.0,
               ),
               Padding(padding: const EdgeInsets.all(3.0),
               child: ElevatedButton(
