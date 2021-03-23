0c71c07d3283ee6788d03e74ccddfc6a31d78983
soumi7 <soumibardhan10@gmail.com>
2021-03-23 10:49:11 +0530

    ?

--- lib/screens/name_screen.dart
+++ lib/screens/name_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:reaction_lab/res/custom_colors.dart';
 import 'dashboard_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NameScreen extends StatelessWidget {
+  final User user;
+  const NameScreen({required this.user});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +22,19 @@ class NameScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Username',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      borderSide: BorderSide(color: Colors.white10),
-                    ),
+              Padding(padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                hintText: 'Username',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12.0))
                   ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide(color: Colors.white10),
                 ),
+                ),
+              )
               ),
               SizedBox(
                 height: 20.0,
@@ -50,8 +53,7 @@ class NameScreen extends StatelessWidget {
               )
             ],
           ),
-        ),
-      ),
+        ),),
     );
   }
 }
