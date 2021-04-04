5ab86a4fe1b9cedc30dbd4770981691836ba97b2
Souvik Biswas <sbis1999@gmail.com>
2021-04-04 09:23:40 +0530

    Chnage color

--- lib/res/custom_colors.dart
+++ lib/res/custom_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomColors {
   static final Color primaryAccent = Color(0xFF6AE2CD);
-  static final Color primaryDark = Color(0xFF181A4B);
+  static final Color primaryDark = Color(0xFF2C2F8C);
   static final Color orangeDark = Color(0xFFFF5A49);
   static final Color orangeLight = Color(0xFFFF855F);
   static final Color yellow = Color(0xFFFFDB8F);
